7f076f245345a8076d681d50505af2f334698535
Ferdinando Micco s290247 <s290247@studenti.polito.it>
2022-05-07 13:44:42 +0200

    added all required by travelerService methods in UserDetailsServic

--- traveler_service/src/main/kotlin/it/polito/wa2/traveler_service/services/impl/UserDetailsService.kt
+++
@@ -1,5 +0,0 @@
-package it.polito.wa2.traveler_service.services.impl
-
-interface UserDetailsService {
-
-}
---
+++ traveler_service/src/main/kotlin/it/polito/wa2/traveler_service/services/impl/UserDetailsServiecImpl.kt
@@ -0,0 +1,48 @@
+package it.polito.wa2.traveler_service.services.impl
+
+import it.polito.wa2.traveler_service.dtos.TicketPurchasedDTO
+import it.polito.wa2.traveler_service.dtos.UserDetailsDTO
+import it.polito.wa2.traveler_service.repositories.UserDetailsRepository
+import it.polito.wa2.traveler_service.services.UserDetailsService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.annotation.EnableAsync
+import org.springframework.scheduling.annotation.EnableScheduling
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+@Transactional
+@Configuration
+@EnableScheduling
+@EnableAsync
+class UserDetailsServiecImpl : UserDetailsService {
+
+    @Autowired lateinit var userDetailsRepository: UserDetailsRepository
+
+    override fun getUserProfile (username: String) : UserDetailsDTO {
+      return UserDetailsDTO("TODO")
+    }
+
+    override fun putUserProfile (userDetailsDTO: UserDetailsDTO) : UserDetailsDTO {
+        return UserDetailsDTO("TODO")
+    }
+
+    override fun getUserTickets (username: String) : List<TicketPurchasedDTO> {
+        return mutableListOf()
+    }
+
+    override fun postUserTickets(tickets : List<TicketPurchasedDTO>) : List<TicketPurchasedDTO> {
+        return mutableListOf()
+    }
+
+    override fun getTravelers(){
+    }
+
+    override fun getTravelerProfile(){
+    }
+
+    override fun getTravelerTickets() {
+    }
+
+}
