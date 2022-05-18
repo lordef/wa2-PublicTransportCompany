642d9a4b481d2d975aafbb81e49a2991f90dee16
lordef <lorenzo.defilippis02@gmail.com>
2022-05-18 19:52:12 +0200

    header test done

--- traveler_service/src/test/kotlin/it/polito/wa2/traveler_service/integration_tests/AuthenticationIntegrationTests.kt
+++ traveler_service/src/test/kotlin/it/polito/wa2/traveler_service/integration_tests/AuthenticationIntegrationTests.kt
@@ -1,7 +1,5 @@
 package it.polito.wa2.traveler_service.integration_tests
 
-import it.polito.wa2.traveler_service.dtos.UserDetailsDTO
-import it.polito.wa2.traveler_service.entities.UserDetails
 import it.polito.wa2.traveler_service.repositories.UserDetailsRepository
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
@@ -9,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
-import org.springframework.boot.test.web.client.postForEntity
 import org.springframework.boot.web.server.LocalServerPort
 import org.springframework.http.HttpEntity
 import org.springframework.http.HttpHeaders
@@ -74,5 +71,29 @@ class AuthenticationIntegrationTests {
     }
 
 
+    @Test
+    fun absentHeaderJwtTest() {
+        val baseUrl = "http://localhost:$port/my/profile"
+        val entity = HttpEntity("")
+        val response = restTemplate.exchange(baseUrl, HttpMethod.GET, entity, String::class.java)
 
+        Assertions.assertEquals(HttpStatus.UNAUTHORIZED, response.statusCode)
+    }
+
+
+    @Test
+    fun noBearerHeaderJwtTest() {
+        val baseUrl = "http://localhost:$port/my/profile"
+
+        val headers = HttpHeaders()
+
+        val entity = HttpEntity("")
+
+        headers.setBasicAuth("eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJjdXN0b21lcjEiLCJpYXQiOjE2NTI4OTE4NzksImV4cCI6MTcxNjA1MTI2Miwicm" +
+                "9sZXMiOlsiQ1VTVE9NRVIiXX0.W71JOUP-TSK_j__yDz3XlWJbtO7UD3_5ZVs7BVQXg2EqKwHeW9J7d9NHpVAOVDpHtTyuuJWoBmA26jQ9wyP78g")
+
+        val response = restTemplate.exchange(baseUrl, HttpMethod.GET, entity, String::class.java)
+
+        Assertions.assertEquals(HttpStatus.UNAUTHORIZED, response.statusCode)
+    }
 }
