189883c152864996c56769d3fc278ad18c27678e
Ferdinando Micco <s290247@studenti.polito.it>
2022-06-02 19:15:54 +0200

    test Payment service Dto and deserializer

--- payment_service/src/test/kotlin/it/polito/wa2/payment_service/unit_tests/DtosUnitTests.kt
+++ payment_service/src/test/kotlin/it/polito/wa2/payment_service/unit_tests/DtosUnitTests.kt
@@ -1,75 +1,105 @@
 package it.polito.wa2.payment_service.unit_tests
 
+import com.fasterxml.jackson.core.JsonParseException
 import it.polito.wa2.payment_service.dtos.TransactionDTO
+import it.polito.wa2.payment_service.dtos.deserializer.PaymentRequestDeserializer
 import it.polito.wa2.payment_service.dtos.toDTO
 import it.polito.wa2.payment_service.entities.Status
 import it.polito.wa2.payment_service.entities.Transaction
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeEach
 import org.springframework.boot.test.context.SpringBootTest
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
+import com.fasterxml.jackson.databind.ObjectMapper
+import it.polito.wa2.payment_service.dtos.PaymentInfoDTO
+import org.apache.kafka.common.errors.SerializationException
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.assertThrows
 
 
+/**
+ *
+ * **/
 @SpringBootTest
 class DtosUnitTests {
 
     @Test
     fun matchingTransactionToDto() {
-        val transactionEntity = Transaction(1, 2f, "testCustomer", 1, LocalDateTime.now(), Status.PENDING, "asbcdefghil", "04-04-2024", "789", "Test Customer" )
+        val transactionEntity = Transaction(
+            1,
+            2f,
+            "testCustomer",
+            1,
+            LocalDateTime.now(),
+            Status.PENDING,
+            "asbcdefghil",
+            "04-04-2024",
+            "789",
+            "Test Customer"
+        )
 
         val formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss")
 
         val date = LocalDateTime.now().format(formatter)
 
-        val expectedTransactionDto = TransactionDTO(1, 2f, "testCustomer", 1, date, Status.PENDING, "asbcdefghil", "04-04-2024", "789", "Test Customer")
+        val expectedTransactionDto = TransactionDTO(
+            1,
+            2f,
+            "testCustomer",
+            1,
+            date,
+            Status.PENDING,
+            "asbcdefghil",
+            "04-04-2024",
+            "789",
+            "Test Customer"
+        )
 
         Assertions.assertEquals(expectedTransactionDto, transactionEntity.toDTO())
     }
 
-// TODO : Test deserializer for PaymentInfoDto https://github.com/magnuspedro/KafkaConsumer/blob/main/src/test/kotlin/com/magnuspedro/kafka/consumer/KafkaConsumer/entities/deserializer/ProductDeserializerTest.kt
+    private val mapper: ObjectMapper = ObjectMapper()
+    private lateinit var paymentRequestDeserializer: PaymentRequestDeserializer
 
-//    @Test
-//    fun matchingUserToDto() {
-//        val userEntity = User("jack", "passW0rD", "email@gmail.com")
-//
-//        val expectedUserDto = UserDTO(null, "jack", "passW0rD", "email@gmail.com",userEntity.roles,false)
-//
-//        Assertions.assertEquals(expectedUserDto, userEntity.toDTO())
-//    }
-//
-//    @Test
-//    fun matchingActivationToDto() {
-//        val userEntity = User("jack", "passW0rD", "email@gmail.com")
-//        val activationEntity = Activation(userEntity)
-//        activationEntity.provisionalUserId = UUID.randomUUID()
-//
-//        val expectedActivationDto = ActivationDTO(activationEntity.provisionalUserId, activationEntity.activationCode)
-//
-//        Assertions.assertEquals(expectedActivationDto, activationEntity.toDTO())
-//    }
-//
-//
-//    @Test
-//    fun matchingActivationWithNullProvisionalUserIdToDto() {
-//        val userEntity = User()
-//        val activationEntity = Activation(userEntity)
-//
-//        val expectedActivationDto = ActivationDTO(null, activationEntity.activationCode)
-//
-//        Assertions.assertEquals(expectedActivationDto, activationEntity.toDTO())
-//    }
-//
-//
-//    @Test
-//    fun notMatchingActivationToDto() {
-//        val userEntity = User()
-//        val activationEntity = Activation(userEntity)
-//
-//        val expectedActivationDto = ActivationDTO(null, null)
-//
-//        Assertions.assertNotEquals(expectedActivationDto, activationEntity.toDTO())
-//
-//    }
+    @BeforeEach
+    fun setup() {
+        paymentRequestDeserializer = PaymentRequestDeserializer()
+    }
+
+    @Test
+    fun validPaymentRequestTest() {
+        val paymentInfoDTO = PaymentInfoDTO(5f, "abcdefghil", "02/2024", "798", "test Customer", "testCustomer", 1)
+
+        val deserializedProduct: PaymentInfoDTO?  = paymentRequestDeserializer.deserialize(
+            "",
+            mapper.writeValueAsBytes(paymentInfoDTO)
+        )
+
+        assertEquals(paymentInfoDTO, deserializedProduct)
+    }
+
+    @Test
+    fun invalidPaymentRequestTest() {
+        val byteArray: ByteArray = "Teste".toByteArray()
+
+        assertThrows<JsonParseException> {
+            paymentRequestDeserializer.deserialize(
+                "",
+                byteArray
+            )
+        }
+    }
+
+    @Test
+    fun nullPaymentRequestTest() {
+        var exception = assertThrows<SerializationException> {
+            paymentRequestDeserializer.deserialize(
+                "product",
+                null
+            )
+        }
+    }
 
 }
