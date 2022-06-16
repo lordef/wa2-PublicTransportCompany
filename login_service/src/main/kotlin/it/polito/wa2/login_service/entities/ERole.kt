161f694b65f5c1e2edd03fbdff1cb78d733ec060
Ferdinando Micco <s290247@studenti.polito.it>
2022-06-16 18:25:40 +0200

    authentication embedded_system working version

--- login_service/src/main/kotlin/it/polito/wa2/login_service/entities/ERole.kt
+++ login_service/src/main/kotlin/it/polito/wa2/login_service/entities/ERole.kt
@@ -4,7 +4,9 @@ import org.springframework.security.core.GrantedAuthority
 
 enum class ERole: GrantedAuthority {
     CUSTOMER,
-    ADMIN;
+    EMBEDDED_SYSTEM,
+    ADMIN,
+    ADMIN_E;
 
     override fun getAuthority(): String {
         return this.name
