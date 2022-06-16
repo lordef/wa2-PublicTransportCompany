4d01c67c839d95a2530b3feb1bd4889bb60f4981
Ferdinando Micco <s290247@studenti.polito.it>
2022-06-16 17:12:38 +0200

    authentication for turnails as embeddedsystem fixed

--- login_service/src/main/resources/data.sql
+++ login_service/src/main/resources/data.sql
@@ -7,14 +7,14 @@ INSERT INTO roles (id, name) VALUES (3, 'ADMIN_E');
     nickname: "startup_admin"
    password : "Password2022!"
 */
-INSERT INTO users (id, active, email, nickname, password) VALUES (1, true, '', 'startup_admin', '$2a$10$tF89eHwXEblVRSeeIUB79e8pcTr5euSFSq8MOz.u0jZmgcToGz8Ha');
+INSERT INTO users (id, active, email, nickname, password) VALUES (1, true, ' ', 'startup_admin', '$2a$10$tF89eHwXEblVRSeeIUB79e8pcTr5euSFSq8MOz.u0jZmgcToGz8Ha');
 INSERT INTO user_role (user_id, role_id) VALUES (1, 3);
 
 /* INSERT STARTUP EMBEDDED IN USER
     nickname: "embedded_system"
    password : "Password2022!"
 */
-INSERT INTO users (id, active, email, nickname, password) VALUES (0, true, '', 'embedded_system', '$2a$10$tF89eHwXEblVRSeeIUB79e8pcTr5euSFSq8MOz.u0jZmgcToGz8Ha');
+INSERT INTO users (id, active, email, nickname, password) VALUES (0, true, ' ', 'embedded_system', '$2a$10$tF89eHwXEblVRSeeIUB79e8pcTr5euSFSq8MOz.u0jZmgcToGz8Ha');
 INSERT INTO user_role (user_id, role_id) VALUES (0, 0);
 
 /*TODO : check if tests have problems with the insertion of startup user*/
